68276ac8ca2abceaf34c67ef6e6d7820c5f94f86
dovanda1997 <dovandaa976@gmail.com>
2023-12-25 11:30:39 +0700

    fixFileScript

--- main.js
+++ main.js
@@ -25,3 +25,29 @@ function toggleQues(element) {
   const content = element.querySelector(".hiden");
   content.style.display = content.style.display === "none" ? "block" : "none";
 }
+
+// Lấy thẻ icon bar và navbar
+const iconBar = document.getElementById("icon-bar");
+const navbar = document.getElementById("navbar");
+
+// Thêm sự kiện click cho icon bar
+iconBar.addEventListener("click", function () {
+  // Kiểm tra trạng thái hiển thị của navbar
+  const navbarDisplay = window
+    .getComputedStyle(navbar)
+    .getPropertyValue("display");
+
+  // Nếu hiện tại đang ẩn, hiển thị navbar, ngược lại ẩn đi
+  if (navbarDisplay === "none") {
+    navbar.style.display = "block";
+  } else {
+    navbar.style.display = "none";
+  }
+});
+function checkWidth() {
+  if (window.innerWidth >= 768) {
+    navbar.style.display = "none";
+  }
+}
+window.addEventListener("load", checkWidth);
+window.addEventListener("resize", checkWidth);
